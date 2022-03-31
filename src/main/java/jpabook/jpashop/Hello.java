8e7f409ab8e9babc89f54139b91c9b8a4e4cde33
curiousKidd <skasrjhaan@gmail.com>
2022-04-01 00:28:03 +0900

    20220331(목) - 김영한 jpa 강의

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+
+@Getter
+@Setter
+public class Hello
+{
+    private String data;
+}
